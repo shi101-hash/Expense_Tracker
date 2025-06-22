4ab51d296846b7c1843eb8cc1f09260e5bc9c2db
shi101-hash <135109233+shi101-hash@users.noreply.github.com>
2025-06-22 11:44:22 +0530

    Add files via upload

---
+++ ExpenseRepository.java
@@ -0,0 +1,10 @@
+package com.project.Expenses.repository;
+
+import com.project.Expenses.model.Expense;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends MongoRepository<Expense, String> {
+
+}
---
+++ ExpenseService.java
@@ -0,0 +1,29 @@
+package com.project.Expenses.service;
+
+import com.project.Expenses.model.Expense;
+import com.project.Expenses.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ExpenseService {
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    public Expense createExpense(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public void deleteExpense(String id) {
+        expenseRepository.deleteById(id);
+    }
+
+    public Expense getExpenseById(String id) {
+        return expenseRepository.findById(id).orElse(null);
+    }
+}
