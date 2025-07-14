cc6d34c4bebcdd8017bde09f3e981db6038a3129
shi101-hash <135109233+shi101-hash@users.noreply.github.com>
2025-07-14 18:35:10 +0530

    Add files via upload

---
+++ ExpenseController.java
@@ -0,0 +1,98 @@
+package com.project.Expenses.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.Expenses.model.Expense;
+import com.project.Expenses.model.User;
+import com.project.Expenses.service.ExpenseService;
+import com.project.Expenses.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/expenses")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String welcomePage(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+
+        }
+        return "welcome";
+    }
+
+    @GetMapping("/list")
+    public String viewExpenses(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        List<Expense> expenses = expenseService.getAllExpenses();
+        model.addAttribute("expenses", expenses);
+        return "expense_list";
+    }
+
+    @GetMapping("/new")
+    public String newExpenseForm(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("expense", new Expense());
+        return "new_expense";
+    }
+
+    @PostMapping
+    public String createExpense(@ModelAttribute Expense expense, Model model, HttpSession session) {
+        try {
+            User user = (User) session.getAttribute("user");
+            if (user == null) {
+                return "redirect:/login";
+            }
+            expenseService.createExpense(expense);
+            return "redirect:/expenses";
+        } catch (Exception e) {
+            model.addAttribute("error", "An error occurred while adding the expense.");
+            return "new_expense";
+        }
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateExpense(@PathVariable String id, Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        Expense expense = expenseService.getExpenseById(id);
+        model.addAttribute("expense", expense);
+        return "new_expense";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteExpense(@PathVariable String id, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        expenseService.deleteExpense(id);
+        return "redirect:/expenses/list";
+
+    }
+}
---
+++ UserService.java
@@ -0,0 +1,20 @@
+package com.project.Expenses.service;
+
+import com.project.Expenses.model.User;
+import com.project.Expenses.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}
