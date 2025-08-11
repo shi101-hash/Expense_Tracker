fbab6bac1fe8189c49be5367890a94521f438cad
shi101-hash <135109233+shi101-hash@users.noreply.github.com>
2025-08-11 18:16:55 +0530

    Add files via upload

---
+++ src/main/java/com/Expenses/repository/ExpenseRepository.java
@@ -0,0 +1,13 @@
+package com.project.Expenses.repository;
+
+import com.project.Expenses.model.Expense;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    List<Expense> findByName(String name);
+
+}
